2e410b276ed0cab8e9002276faf1e5500ea3db87
trovatojcp <trovato.jcp@gmail.com>
2020-03-10 15:03:15 +0100

    Suppression 2 fichiers

---
+++ js/script.js
@@ -0,0 +1,75 @@
+'use strict'
+  // cache les images
+
+/*************************************************************************************************/
+/* ****************************************** DONNEES ****************************************** */
+/*************************************************************************************************/
+// Variable (tableau) qui contient toutes les <figure>
+const figure = document.querySelectorAll('figure');
+// Variable qui continent le chevron droit
+const flecheDroite = document.querySelector('.fas.fa-chevron-right');
+// Variable qui contient le chevron gauche
+const flecheGauche = document.querySelector('.fas.fa-chevron-left');
+let i = 0;
+
+
+/*************************************************************************************************/
+/* ***************************************** FONCTIONS ******************************************/
+/*************************************************************************************************/
+// Fonction Next
+
+function next() {
+  console.log(i);
+  figure[i].classList.remove('active');
+  i++;
+  if (i >= figure.length) {
+    i = 0;
+  }
+  figure[i].classList.add('active');
+}
+
+// Fonction Preview
+
+function preview() {
+  console.log(i);
+
+  figure[i].classList.remove('active');
+  i--;
+  if (i < 0) {
+    i = figure.length - 1;
+  }
+  figure[i].classList.add('active');
+}
+
+// Fonction pour les touches
+
+function onKeyDown(e) {
+  switch (e.keyCode) {
+    case 37:
+      preview();
+      break;
+    case 39:
+      next();
+      break;
+  }
+}
+
+/*************************************************************************************************/
+/* ************************************** CODE PRINCIPAL *************************************** */
+/*************************************************************************************************/
+
+// évènement pour ajouter la classe 'active' a la figure
+//boucle pour attribuer l'evt à toutes les case de la variable figure
+for (let j = 0; j < figure.length; j++) {
+  flecheDroite.addEventListener('click', next);
+  flecheGauche.addEventListener('click', preview);
+}
+// Evenement (fonction 'onKeyDown' sur les touches)
+document.addEventListener('keydown', onKeyDown);
+
+
+/************************************************************************
+ **********************************************************************/
+/**********************************************TEST ********************/
+console.log(document.getElementsByTagName('figure').length);
+console.log(figure);
